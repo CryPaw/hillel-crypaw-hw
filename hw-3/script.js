3f34fee37c14d0e43739347cfb41fe0ec3c47300
Nick <119486388+CryPaw@users.noreply.github.com>
2023-02-12 19:50:43 +0200

    Update script.js

--- hw-3/script.js
+++ hw-3/script.js
@@ -6,7 +6,7 @@ const d = a - b;
 const g = a * b;
 const f = a / b;
 
-alert (c);
-alert (d);
-alert (g);
-alert (f);
+alert   ((a + '+' + b + '=' + c + ' \n')+
+        (a + '-' + b + '=' + d + ' \n')+
+        (a + '*' + b + '=' + g + ' \n')+
+        (a + '/' + b + '=' + f));
